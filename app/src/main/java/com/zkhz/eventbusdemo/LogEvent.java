29758471a4645e12a2adfd4ee0e4cb41d7361da5
USER-20170614UA\Administrator <wangk0612@126.com>
2017-12-28 17:46:47 +0800

    activity]

---
+++ app/src/main/java/com/zkhz/eventbusdemo/LogEvent.java
@@ -0,0 +1,22 @@
+package com.zkhz.eventbusdemo;
+
+/**
+ * Created by Administrator on 2017/12/28 0028.
+ */
+
+public class LogEvent {
+
+    private String text;
+
+    public LogEvent(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
